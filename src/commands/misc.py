b9353f5e238735c5e29cf336608f7fd34859171e
KrashKart <e1113755@u.nus.edu>
2025-10-07 12:04:19 +0800

    Fix send_message functionality not parsing newline properly

--- src/commands/misc.py
+++ src/commands/misc.py
@@ -6,11 +6,19 @@ from telebot.types import Message
 
 logger = logging.getLogger(__name__)
 
+def _send_message_to_group(bot: TeleBot, message: Message, group: int) -> None:
+    try:
+        text = ' '.join(message.html_text.split(' ')[1:])
+        bot.send_message(group, text, parse_mode="HTML")
+    except Exception as e:
+        send_log_message(bot, f"Error in {function.__name__}: {e}")
+        logger.error(f"Error in {function.__name__}: {e}")
+
+
 def send_message_admin(bot: TeleBot, message: Message, groups: dict) -> None:
     try:
         if message.chat.id == groups["ADMIN_GROUP"]["id"]:
-            text = ' '.join(message.html_text.split()[1:])
-            bot.send_message(groups["ADMIN_GROUP"]["id"], text, parse_mode="HTML")
+            _send_message_to_group(bot, message, groups["ADMIN_GROUP"]["id"])
         else:
             bot.send_message(message.chat.id, f"You are not allowed to use this in {message.chat.title}")
     except Exception as e:
@@ -20,8 +28,7 @@ def send_message_admin(bot: TeleBot, message: Message, groups: dict) -> None:
 def send_message_recre(bot: TeleBot, message: Message, groups: dict) -> None:
     try:
         if message.chat.id == groups["ADMIN_GROUP"]["id"]:
-            text = ' '.join(message.html_text.split()[1:])
-            bot.send_message(groups["RECRE_GROUP"]["id"], text, parse_mode="HTML")
+            _send_message_to_group(bot, message, groups["RECRE_GROUP"]["id"])
         else:
             bot.send_message(message.chat.id, f"You are not allowed to use this in {message.chat.title}")
     except Exception as e:
